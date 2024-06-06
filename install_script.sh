e66b0aa29df7bc0540e9a8b872fecf522d935101
root <root@debian>
2024-06-06 15:48:51 +0530

    Replaced 'com.amazon' with business names: com.amazon com.bincore

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the array of business names
-BUSINESS_NAMES=("com.amazon" "com.bincore")
+BUSINESS_NAMES=("com.bincore" "com.bincore")
 
 # Print the current working directory
 echo "Running script in directory: $(pwd)"
@@ -15,10 +15,10 @@ escape_periods() {
 for BUSINESS_NAME in "${BUSINESS_NAMES[@]}"; do
   BUSINESS_NAME_ESCAPED=$(escape_periods "$BUSINESS_NAME")
   
-  echo "Replacing com.amazon with ${BUSINESS_NAME} in all files..."
+  echo "Replacing com.bincore with ${BUSINESS_NAME} in all files..."
   
-  # Find and replace "com.amazon" with the current business name in all files
-  find . -type f -exec sed -i "s/com\.blabber/${BUSINESS_NAME_ESCAPED}/g" {} +
+  # Find and replace "com.bincore" with the current business name in all files
+  find . -type f -exec sed -i "s/com\.amazon/${BUSINESS_NAME_ESCAPED}/g" {} +
 
   echo "Replacement with ${BUSINESS_NAME} complete."
 done
@@ -27,7 +27,7 @@ done
 git add .
 
 # Commit the changes
-git commit -m "Replaced 'com.amazon' with business names: ${BUSINESS_NAMES[*]}"
+git commit -m "Replaced 'com.bincore' with business names: ${BUSINESS_NAMES[*]}"
 
 # Push the changes to the remote repository
 git push origin main
