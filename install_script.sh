e5dea7269495b304981bffafedd5b92caee9bd4d
root <root@debian>
2024-06-06 15:57:22 +0530

    Replaced 'com.blabber' with business names: binmile.com bincore.com

--- install_script.sh
+++ install_script.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # Define the array of business names without the 'com.' prefix
-BUSINESS_NAMES=("amazon.com" "binmile.com")
+BUSINESS_NAMES=("binmile.com" "bincore.com")
 
 # Print the current working directory
 echo "Running script in directory: $(pwd)"
 
+# Function to replace com.blabber with the business name
+replace_com_blabber() {
+    sed -i "s/\bcom\.amazon\b/${1}/g" "$2"
+}
+
 # Loop through each business name and perform the replacement
 for BUSINESS_NAME in "${BUSINESS_NAMES[@]}"; do
-  echo "Replacing com.blabber with ${BUSINESS_NAME} in all files..."
+    echo "Replacing com.blabber with ${BUSINESS_NAME} in all files..."
   
-  # Find and replace "com.blabber" with the current business name in all files
-  find . -type f -exec sed -i "s/com\.bincore/${BUSINESS_NAME}/g" {} +
+    # Find and replace "com.blabber" with the current business name in all files
+    find . -type f -exec bash -c 'replace_com_blabber "$1" "$0"' {} "$BUSINESS_NAME" \;
 
-  echo "Replacement with ${BUSINESS_NAME} complete."
+    echo "Replacement with ${BUSINESS_NAME} complete."
 done
 
 # Add changes to git
